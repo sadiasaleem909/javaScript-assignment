2b9308f39ff8d4bf641485de94cf5c21f00389cd
sadiasaleem909 <saleemsadia092@gmail.com>
2026-07-28 00:32:15 +0500

    6t09 complet

---
+++ chapter6 to9/app.js
@@ -0,0 +1,29 @@
+
+// Question 1
+// var a = 10;
+// document.write("Result:" + "<br/>");
+// document.write("The value of a is: " + a + "<br/><br/>");
+// document.write("The value of ++a is: " + ++a + "<br/>");
+// document.write("Now  the value of a is: " + a + "<br/><br/>");
+// document.write("The value of a++ is: " + a++ + "<br/>");
+// document.write("Now  the value of a is: " + a + "<br/><br/>");
+// document.write("The value of --a is: " + --a + "<br/>");
+// document.write("Now  the value of a is: " + a + "<br/><br/>");
+// document.write("The value of a-- is: " + a-- + "<br/>");
+// document.write("Now  the value of a is: " + a + "<br/><br/>");
+
+// Question 2
+// var a = 2; b = 1;
+// document.write("--a is: " + --a + "<br/>");
+// document.write("--a - --b is: " + (--a - --b) + "<br/>");
+// document.write("--a - --b + ++b is: " + (--a - --b + ++b) + "<br/>");
+// document.write("--a - --b + ++b + b-- is: " + (--a - --b + ++b + b--) + "<br/>");
+
+
+// question3
+// var name =prompt("write your name");
+// document.write("Hi " + name)
+
+// question4
+var num =prompt("enter a number, 5");
+document.write("Multiplication table of " + num + "<br/><br/>");
